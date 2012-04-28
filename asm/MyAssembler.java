277010d44ad8913bde8f29f6e17380be1171c04a
Kevin Chon <kkc1190@gmail.com>
2012-04-27 18:05:17 -0700

    removed abstract from method headers

--- asm/MyAssembler.java
+++ asm/MyAssembler.java
@@ -5,20 +5,23 @@ class MyAssembler extends Assembler
 {
 
     // The student has to implement it for processing the labels.
-    abstract void processLabel(String sourceCode);
+    void processLabel(String sourceCode);
 	
     // The student has to implement it for generating the machine codes.
-    abstract String generateCode(Instruction instruction);
+    String generateCode(Instruction instruction);
 	
     // The student has to implement it for updating the program counter.
-    abstract void updateProgramCounter(Instruction instruction);
+    void updateProgramCounter(Instruction instruction);
 	
     // The student has to implement it for initializing some of their own variables and data structures.
-    abstract void initialization()  throws IOException;	
+    void initialization()  throws IOException
+	{
+	
+	}
 	
     // The student has to implement it for replacing the labels used in instruction
-    abstract void replaceInstructionLabel(Instruction instruction);
+    void replaceInstructionLabel(Instruction instruction);
 	
     // The student has to implement it for replacing the labels used in memory
-    abstract void replaceMemoryLabel();
+    void replaceMemoryLabel();
 }
